c6fdbdf1e070fd0bca992821f6829a68ac785138
Timm Hirsens <timmhirsens@gmail.com>
2021-09-19 11:50:25 +0200

    systray?

--- config.def.h
+++ config.def.h
@@ -36,7 +36,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Gimp",     		NULL,       NULL,       0,            1,           -1 },
+	{ "stalonetray",     NULL,       NULL,       ~0,            1,           -1 },
+	{ "trayer",     NULL,       NULL,       ~0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
