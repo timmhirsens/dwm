45fc8dd22212a38adfe286802368165d522eef6a
Timm Hirsens <timmhirsens@gmail.com>
2021-10-06 22:51:54 +0200

    centered master

--- config.def.h
+++ config.def.h
@@ -62,6 +62,8 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
