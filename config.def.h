653e5e9ee524a79ac32c8c692415db5eea9b0324
Timm Hirsens <timmhirsens@gmail.com>
2021-10-06 22:31:40 +0200

    Revert "fibonnaci"
    This reverts commit e14d12f7c03b8a04ecc6a4ec515ae805c520d3d3.

--- config.def.h
+++ config.def.h
@@ -56,7 +56,6 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
-#include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
@@ -64,8 +63,6 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 	{ "|M|",      centeredmaster },
 	{ ">M>",      centeredfloatingmaster },
- 	{ "[@]",      spiral },
- 	{ "[\\]",      dwindle },
 	{ NULL,       NULL },
 };
 
